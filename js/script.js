1ed3fb27a7ff8a431740ad4991f113f833551f06
Bogdan Tkach <tkachbogdan0205@gmail.com>
2022-04-25 20:43:55 +0300

    Start js 'Find BMI result'

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-const input = document.querySelectorAll("number_button" );
+const givenNumbers = document.querySelectorAll(".number_button" );
 const resultField= document.querySelector();
 let BMI_result = 0;
 
-for (let i = 0; i < input.length; i++) {
-    input[i].addEventListener('number-button', function (){
+for (let i = 0; i < givenNumbers.length; i++) {
+    givenNumbers[i].addEventListener('.number-button', function (){
         if (firstInput.value !== '' && secondInput.value !== '' ) {
             BMI_result = secondInput / ((firstInput / 100)** 2)
             resultField.textContent = BMI_result;
