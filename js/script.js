36427cb7f8023cc828a1566bbaf76b27e6bae437
Bogdan Tkach <tkachbogdan0205@gmail.com>
2022-04-29 02:18:47 +0300

    JS changes

--- js/script.js
+++ js/script.js
@@ -3,9 +3,6 @@ const calculate_button = document.querySelector(".calculate-button")
 calculate_button.addEventListener('click', BMI_calculation)
 
 
-
-
-
 function BMI_calculation () {
     let bmi;
     let height = parseInt(document.getElementById("height").value)
@@ -14,32 +11,31 @@ function BMI_calculation () {
     const warning_for_height = document.querySelector(".warning_for_height")
     const warning_for_weight = document.querySelector(".warning_for_weight")
 
-    if (height === "" || isNaN(height)) {
-       warning_for_height.innerHTML = "Please enter height!"
-       document.getElementById("height").style.border = "2px solid red"
 
-    } else if (weight === "" || isNaN(weight)) {
-        warning_for_weight.innerHTML = "Please enter weight!"
+
+    if (weight === "" || isNaN(weight)) {
+        warning_for_weight.textContent = "Please enter weight!"
         document.getElementById("weight").style.border = "2px solid red"
+
+    } else if (height === "" || isNaN(height)) {
+        warning_for_height.textContent = "Please enter height!"
+        document.getElementById("height").style.border = "2px solid red"
+
     } else {
-        bmi = (weight / ((height / 100) **2)).toFixed(2)
-        result.innerHTML = bmi;
+        bmi = (weight / ((height / 100) ** 2)).toFixed(2)
+        result.textContent = bmi;
     }
 
     const status = document.getElementById('status')
 
-    switch (true) {
-        case (bmi < 18.5):
-            status.innerText = 'Underweight';
-        break;
-        case (18.5 <= bmi && bmi <= 24.9):
-            status.innerText = 'Normal weight';
-        break;
-        case (25.0 <= bmi && bmi <= 29.9):
-            status.innerText = 'Overweight';
-        break;
-        case (bmi > 30.0):
-            status.innerText = 'Overweight';
-        break;
+
+    if (bmi < 18.5) {
+        status.innerText = 'Underweight';
+    } else if (18.5 <= bmi && bmi <= 24.9) {
+        status.innerText = 'Healthy weight';
+    } else if (25.0 <= bmi && bmi <= 29.9) {
+        status.innerText = 'Overweight';
+    } else if (bmi > 30.0) {
+        status.innerText = 'Obese';
     }
 }
