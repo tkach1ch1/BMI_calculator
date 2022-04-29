c49ecc8a3547c166abfcdb6e43a90533dae3ecc2
Bogdan Tkach <tkachbogdan0205@gmail.com>
2022-04-29 03:15:11 +0300

    JS changes

--- js/script.js
+++ js/script.js
@@ -2,30 +2,42 @@
 const calculate_button = document.querySelector(".calculate-button")
 calculate_button.addEventListener('click', BMI_calculation)
 
+const warning_for_height = document.querySelector(".warning_for_height")
+const warning_for_weight = document.querySelector(".warning_for_weight")
 
 function BMI_calculation () {
     let bmi;
     let height = parseInt(document.getElementById("height").value)
     let weight = parseInt(document.getElementById("weight").value)
     const result = document.getElementById("result");
-    const warning_for_height = document.querySelector(".warning_for_height")
-    const warning_for_weight = document.querySelector(".warning_for_weight")
 
+
+
+
+ //BMI_calculation
     if (height === "" || isNaN(height)) {
        warning_for_height.innerHTML = "Please enter height!"
        document.getElementById("height").style.border = "2px solid red"
+    }
 
-    } else if (weight === "" || isNaN(weight)) {
+
+    if (weight === "" || isNaN(weight)) {
         warning_for_weight.innerHTML = "Please enter weight!"
         document.getElementById("weight").style.border = "2px solid red"
-    } else {
+    }
+    if (weight !== '' && height !== '') {
         bmi = (weight / ((height / 100) **2)).toFixed(2)
         result.innerHTML = bmi;
     }
 
+    //Check result on NAN
+    if (isNaN(result)){
+        result.innerText = "0";
+    }
+
     const status = document.getElementById('status')
 
-
+// BMI_status
     if (bmi < 18.5) {
         status.innerText = 'Underweight';
     } else if (18.5 <= bmi && bmi <= 24.9) {
@@ -36,3 +48,10 @@ function BMI_calculation () {
         status.innerText = 'Obese';
     }
 }
+
+document.addEventListener('click', removeWarning)
+//
+// function removeWarning () {
+//     warning_for_height.innerHTML = ""
+//     document.getElementById("height").style.border = null;
+// }
